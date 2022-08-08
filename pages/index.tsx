5a078cf01aa4e81b8da004f413211152bd73df86
BogdanMyagkiy <myagkiybogdan@gmail.com>
2022-08-08 17:24:36 +0300

    CMS bind added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,8 @@ import type { GetServerSideProps, GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
 import client from '../contentful/intex';
 
-const Home = ({ title }: { title: string }) => {
+const Home = ({ homepage }: { homepage: any }) => {
+  console.log(homepage);
   return (
     <div>
       <Head>
@@ -11,7 +12,8 @@ const Home = ({ title }: { title: string }) => {
       </Head>
 
       <main>
-        <h1>{title}</h1>
+        {/* Тайтл берется из CMS */}
+        <h1>{homepage.fields.title}</h1>
       </main>
     </div>
   );
@@ -21,10 +23,17 @@ export default Home;
 
 // для статической генерации html из js - выполняется на стадии билда, выполняется до генерации страницы, генерирует index.html в .next/server/pages/index.html
 export const getStaticProps: GetStaticProps = async () => {
-  const homepage = client.getEntries({});
+  // Позволяет передать query - объект определенного типа, у которого указать набор параметров, по которому запрашивать контент-тайпы
+  const homepage = await client.getEntries({
+    // берется в разделе инфо homepage в contentfull limit, items можно получить через console.log(homepage); и посмотрев что там внутри
+    content_type: 'homepage',
+    limit: 1,
+  });
+  // Первый элемент массива items в массиве homePage
+  const [homePage] = homepage.items;
   return {
     props: {
-      title: 'My new Next.js title',
+      homepage: homePage,
     },
     // указывает через сколько нужно обновить контент на странице
     revalidate: 3000,
