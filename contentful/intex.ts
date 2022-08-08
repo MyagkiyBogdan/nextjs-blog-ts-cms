a72f1c471690e9bc9c9622a81cca5b6d7e2fa477
BogdanMyagkiy <myagkiybogdan@gmail.com>
2022-08-08 17:46:50 +0300

    fixed

--- contentful/intex.ts
+++ contentful/intex.ts
@@ -3,8 +3,10 @@ import { createClient } from 'contentful';
 // позволяет запрашивать контент из CMS по API по GraphQL
 
 const client = createClient({
-  space: String(process.env.SPACE),
-  accessToken: String(process.env.ACCESS_TOKEN),
+  // space: String(process.env.SPACE),
+  // accessToken: String(process.env.ACCESS_TOKEN),
+  space: '57jaehl2fmxx',
+  accessToken: '_dBE5E_thxTqsbl4hA9FSjFrzMJZeUmUGbICD86i7Ss',
 });
 
 export default client;
